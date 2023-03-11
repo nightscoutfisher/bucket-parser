dbf62ca9f253ddf4e6c91a4a3324d875b1af6393
Mike Fisher <mike@fishscalability.com>
2023-03-11 17:55:03 -0500

    added pdf file type

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import functions_framework
+from google.cloud import storage
+from google.cloud import vision
+import pandas as pd
+import json
+import re
 
 
 # Triggered by a change in a storage bucket
 @functions_framework.cloud_event
-def hello_gcs(cloud_event):
+def parse_bucket(cloud_event):
     data = cloud_event.data
 
     event_id = cloud_event["id"]
@@ -22,3 +27,98 @@ def hello_gcs(cloud_event):
     print(f"Metageneration: {metageneration}")
     print(f"Created: {timeCreated}")
     print(f"Updated: {updated}")
+
+    storage_client = storage.Client()
+    bucket_obj = storage_client.bucket(bucket) # email-attachment-test is defined in deploy file
+    blob = bucket_obj.get_blob(name)
+    bt = blob.download_as_string()
+
+    # determine file type
+    file_ext = str(name)[-3:]
+    print(f"Extension: {file_ext}")
+    
+    match file_ext:
+        case "txt":
+            print(bt)
+        case "csv":
+            s = str(bt, "utf-8")
+            s = StringIO(s)
+            df = pd.read_csv(s)
+            print(df)
+        case "pdf":
+            async_detect_document("gs://email-attachment-test/mikefisher/Michael_Fisher_bio.pdf", "gs://email-attachment-test/mikefisher/ocr_results/")
+            write_to_text("gs://email-attachment-test/mikefisher/ocr_results/")
+        case _:
+            print(f"File type not handled")
+ 
+
+def async_detect_document(gcs_source_uri, gcs_destination_uri):
+    """OCR with PDF/TIFF as source files on GCS"""
+
+    # Supported mime_types are: 'application/pdf' and 'image/tiff'
+    mime_type = 'application/pdf'
+
+    # How many pages should be grouped into each json output file.
+    batch_size = 2 
+
+    client = vision.ImageAnnotatorClient()
+
+    feature = vision.Feature(
+        type_=vision.Feature.Type.DOCUMENT_TEXT_DETECTION)
+
+    gcs_source = vision.GcsSource(uri=gcs_source_uri)
+    input_config = vision.InputConfig(
+        gcs_source=gcs_source, mime_type=mime_type)
+
+    gcs_destination = vision.GcsDestination(uri=gcs_destination_uri)
+    output_config = vision.OutputConfig(
+        gcs_destination=gcs_destination, batch_size=batch_size)
+
+    async_request = vision.AsyncAnnotateFileRequest(
+        features=[feature], input_config=input_config,
+        output_config=output_config)
+
+    operation = client.async_batch_annotate_files(
+        requests=[async_request])
+
+    print('Waiting for the operation to finish.')
+    operation.result(timeout=420)
+
+def write_to_text(gcs_destination_uri):
+    # Once the request has completed and the output has been
+    # written to GCS, we can list all the output files.
+    storage_client = storage.Client()
+
+    match = re.match(r'gs://([^/]+)/(.+)', gcs_destination_uri)
+    bucket_name = match.group(1)
+    prefix = match.group(2)
+
+    bucket = storage_client.get_bucket(bucket_name)
+
+    # List objects with the given prefix, filtering out folders.
+    blob_list = [blob for blob in list(bucket.list_blobs(
+        prefix=prefix)) if not blob.name.endswith('/')]
+    print('Output files:')
+    for blob in blob_list:
+        print(blob.name)
+
+    # Process the first output file from GCS.
+    # Since we specified batch_size=2, the first response contains
+    # the first two pages of the input file.
+    output = blob_list[0]
+
+    json_string = output.download_as_string()
+    response = json.loads(json_string)
+
+    # The actual response for the first page of the input file.
+    first_page_response = response['responses'][0]
+    annotation = first_page_response['fullTextAnnotation']
+
+    # Here we print the full text from the first page.
+    # The response contains more information:
+    # annotation/pages/blocks/paragraphs/words/symbols
+    # including confidence scores and bounding boxes
+    print('Full text:\n')
+    print(annotation['text'])
+
+
